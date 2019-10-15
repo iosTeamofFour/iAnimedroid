3db590df143bd20101b7bb885bbba4733349e0f9
Nemesiss.Lin <353388639@qq.com>
2019-10-15 22:12:43 +0800

    完成登陆注册基本界面”

---
+++ app/src/main/java/com/nemesiss/ianime/Activity/IndexActivity.java
@@ -0,0 +1,14 @@
+package com.nemesiss.ianime.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.nemesiss.ianime.R;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
